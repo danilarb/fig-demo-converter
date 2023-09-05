6aacc7ff1081a4056464f95d580592b43913c533
Danila Bublik <dbublik11@gmail.com>
2023-09-05 12:21:06 +1000

    Uupdate helper functions

--- helpers.py
+++ helpers.py
@@ -7,6 +7,7 @@ import json
 import os
 from time import time
 from typing import Any
+from datetime import datetime
 
 import requests
 from dotenv import load_dotenv
@@ -16,19 +17,36 @@ from OAuth2 import oauth
 load_dotenv()
 
 # Constants
-FARM_ID = os.environ['FARM_ID']
+FARM_ID = os.environ['FARM_SHORTCODE_ID']
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 API_URL = os.environ['API_URL']
 TOKEN_URL = os.environ['TOKEN_URL']
 AUTH_URL = os.environ['AUTH_URL']
 REDIRECT_URL = os.environ['REDIRECT_URI']
+FARM_COUNTRY = os.environ['FARM_COUNTRY']
 
 # Global Variables
-oauth_details = None
+OAUTH_DETAILS = None
+CURRENT_YEAR = None
 
 
-def get_headers():
+def get_current_year() -> int:
+    """
+    Returns an integer of the current year.
+    :return: Current year int.
+    """
+    global CURRENT_YEAR
+
+    if CURRENT_YEAR is not None:
+        return CURRENT_YEAR
+
+    CURRENT_YEAR = datetime.now().year
+
+    return CURRENT_YEAR
+
+
+def get_headers() -> dict:
     """
     Returns a dictionary of headers for API requests.
     :return: Headers dictionary.
@@ -39,20 +57,20 @@ def get_headers():
     }
 
 
-def get_oauth_details():
+def get_oauth_details() -> dict | None:
     """
     Returns the OAuth2 details from the JSON file.
     :return: Token dictionary if successful, None otherwise.
     """
-    global oauth_details
+    global OAUTH_DETAILS
 
-    if oauth_details is not None:
-        return oauth_details
+    if OAUTH_DETAILS is not None:
+        return OAUTH_DETAILS
 
     try:
-        with open('OAuth2/oauth.json', 'r') as file:
-            oauth_details = json.load(file)
-            return oauth_details
+        with open('OAuth2/oauth.json', 'r', encoding='utf-8') as file:
+            OAUTH_DETAILS = json.load(file)
+            return OAUTH_DETAILS
     except FileNotFoundError:
         print("OAuth JSON file not found.")
     except json.JSONDecodeError:
@@ -61,18 +79,18 @@ def get_oauth_details():
     return None
 
 
-def get_access_token():
+def get_access_token() -> str | None:
     """
     Returns the access token from the OAuth2 details.
     :return: Access token value if successful, None otherwise.
     """
-    global oauth_details
+    global OAUTH_DETAILS
 
-    if oauth_details is None:
-        oauth_details = get_oauth_details()
+    if OAUTH_DETAILS is None:
+        OAUTH_DETAILS = get_oauth_details()
 
-    if oauth_details is not None:
-        return oauth_details.get('access_token')
+    if OAUTH_DETAILS is not None:
+        return OAUTH_DETAILS.get('access_token')
 
     return None
 
@@ -95,6 +113,8 @@ def get_url(api_type: str, path_vars: list = None) -> str:
             return f'{API_URL}/farms/{FARM_ID}/livestock/transactions'
         case 'livestock_account_mappings':
             return f'{API_URL}/farms/{FARM_ID}/livestock/{path_vars[0]}/account_mappings'
+        case 'cashflow':
+            return f'{API_URL}/farms/{FARM_ID}/reports/cashflow'
 
 
 def get_api(api_type: str, query_params: dict = None, path_vars: list = None) -> Any | None:
@@ -119,8 +139,8 @@ def refresh_token_if_expired(force_refresh: bool = False):
     """
     Checks if the access token is still valid and refreshes it if necessary.
     """
-    global oauth_details
-    if force_refresh or oauth_details.get('expires_at', 0) <= time():
+    global OAUTH_DETAILS
+    if force_refresh or OAUTH_DETAILS.get('expires_at', 0) <= time():
         oauth.refresh_token()
 
 
@@ -128,5 +148,15 @@ def reset_oauth_details():
     """
     Resets the OAuth2 details.
     """
-    global oauth_details
-    oauth_details = None
+    global OAUTH_DETAILS
+    OAUTH_DETAILS = None
+
+
+def find_dictionary(outer_dict, target_dict) -> dict | None:
+    """
+    Finds a dictionary inside a dictionary
+    """
+    for key, inner_dict in outer_dict.items():
+        if inner_dict == target_dict:
+            return key
+    return None
